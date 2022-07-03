02e46e15e1f909820accf9000b8beb0851b350e7
Threat <86489721+AppropriateThreat@users.noreply.github.com>
2022-07-03 18:27:21 +0300

    Add files via upload

--- TicTacTow/Source.cpp
+++ TicTacTow/Source.cpp
@@ -4,61 +4,54 @@
 #define LOG(x) std::cout << x << std::endl
 #define log(x) std::cout << x
 
-char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+char board[9] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-void TitleDrop()
-{
-	system("color a");
-	LOG(" __      __         .__                                         __");
-	LOG("/  \    /  \  ____  |  |    ____   ____    _____    ____      _/  |_  ____                                                                                  ");
-	LOG("\   \/\/   /_/ __ \ |  |  _/ ___\ /  _ \  /     \ _/ __ \     \   __\/  _ \                                                                                 ");
-	LOG(" \        / \  ___/ |  |__\  \___(  <_> )|  Y Y  \\  ___/      |  | (  <_> )                                                                                ");
-	LOG("  \__/\  /   \___  >|____/ \___  >\____/ |__|_|  / \___  >     |__|  \____/                                                                                 ");
-	LOG("       \/        \/            \/              \/      \/                                                                                                   ");
-	LOG("                                                                                                                                                            ");
-	LOG("                                                                                    ___________.__       ___________              ___________               ");
-	LOG("                                                                                    \__    ___/|__|  ____\__    ___/_____     ____\__    ___/____    ____   ");
-	LOG("                                                                                      |    |   |  |_/ ___\ |    |   \__  \  _/ ___\ |    |  /  _ \ _/ __ \  ");
-	LOG("                                                                                      |    |   |  |\  \___ |    |    / __ \_\  \___ |    | (  <_> )\  ___/  ");
-	LOG("                                                                                      |____|   |__| \___  >|____|   (____  / \___  >|____|  \____/  \___  > ");
-	LOG("                                                                                                        \/               \/      \/                     \/  ");
-	LOG("                                                                                                                                                            ");
-	Sleep(3);
-}
-
-void DrawBoard(char choice)
+void DrawBoard()
 {
 	std::cout << "     |     |     \n";
-	std::cout << "  " << board[0][0] << "  | " << board[0][1] << " "
-			  << "  |  " << board[0][2] << "\n";
+	std::cout << "  " << board[0] << "  | " << board[1] << " "
+		<< "  |  " << board[2] << "\n";
 	std::cout << "_____|_____|_____\n";
 	std::cout << "     |     |     \n";
-	std::cout << "  " << board[1][0] << "  | " << board[1][1] << " "
-			  << "  |  " << board[1][2] << " \n";
+	std::cout << "  " << board[3] << "  | " << board[4] << " "
+		<< "  |  " << board[5] << " \n";
 	std::cout << "_____|_____|_____\n";
 	std::cout << "     |     |     \n";
-	std::cout << "  " << board[2][0] << "  | " << board[2][1] << " "
-			  << "  |  " << board[2][2] << " \n";
+	std::cout << "  " << board[6] << "  | " << board[7] << " "
+		<< "  |  " << board[8] << " \n";
 	std::cout << "     |     |     \n";
 }
 
 int main()
 {
-	TitleDrop();
 	char PlayerChoice;
-	do
+
+	LOG("Please choose a sign. (X or 0)");
+	std::cin >> PlayerChoice;
+	if (PlayerChoice == 'X' || PlayerChoice == 'x')
 	{
-		LOG("Please choose a sign. (X or 0)");
-		std::cin >> PlayerChoice;
-		if (PlayerChoice == 'X' || PlayerChoice == 'x')
+		LOG("You will play as Player 1");
+		Sleep(5);
+		system("cls");
+		LOG("\nPlayer 1 [X] VS. Player 2 [0]");
+		DrawBoard();
+
+		int PlayerMove;
+		std::cout << "Your move: ";
+		std::cin >> PlayerMove;
+		switch (PlayerMove)
 		{
-			system("cls");
-			DrawBoard(PlayerChoice);
+		case 1:
+			board[0] = PlayerMove;
+			DrawBoard();
+			break;
 		}
-		else if (PlayerChoice == '0')
-		{
-			system("cls");
-			DrawBoard(PlayerChoice);
-		}
-	} while (PlayerChoice != 'X' || PlayerChoice != '0');
+	}
+
+	else
+	{
+		LOG("You will play as Player 2");
+		system("cls");
+		DrawBoard();
+	}
 }
