bd1f11f837cb60e7a901fbdd3daf61e40d17a146
Paul <paulflorentin.pf@gmail.com>
2023-01-19 07:57:53 +0200

    Added new menu class

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,15 +13,51 @@ using namespace std;
 vector<string> boardValues(10);
 //***************************************************************************************************************************************************************************************
 //										Design sheet
-// create the environment, in this case, the board with input placeholders
-// create a menu and ASCII art for the beginning of the game, end of the game, win, lose and draw screen
+// create the environment, in this case, the board with input placeholders [x]
+// create a menu [x]
+// create ASCII art for the beginning of the game [x], end of the game, win, lose and draw screen
 // create a main menu with several options
 // create a function that draws the board; [x]
-// function that updates the board;
-// create a player choice for the player to choose between player 1 and player 2, and beginning the game according to the game rules: x starts first and 0 starts second
-// create a function that prints the rules
+// function that updates the board; [x]
+// create a player choice for the player to choose between player 1 and player 2, and beginning the game according to the game rules: x starts first and 0 starts second [x]
+// create a function that prints the rules [x]
 // the main loop will be the player that enters a value between 0 and 8 and the symbol will be printed in the placeholder, followed by the precomputed move, until the board is filled
 //***************************************************************************************************************************************************************************************
+
+//menu class implementation
+
+class MenuClass {
+public:
+	void ShowRules()
+	{
+		cout << "This is an interpretation of the Classic TicTacToe game, where 2 players duel in a 4x4 square matrix.\n The goal of tic-tac-toe is to be the first player to get three in a row on a 3-by-3 grid or four in a row in a 4-by-4 grid. \n To start, one player draws a board, creating a grid of squares, usually 3 - by - 3 or 4 - by - 4. \n In a 3-by-3 grid game, the player who is playing 'X' always goes firsmenu. Players alternate placing Xs and Os on the board until either player has three in a row, horizontally, vertically, or diagonally or until all squares on the grid are filled. \n If a player is able to draw three Xs or three Os in a row, then that player wins. If all squares are filled and neither player has made a complete row of Xs or Os, then the game is a draw.";
+	}
+
+	void DisplayMenu() {
+		TextTable menu('-', '|', '+');
+		menu.add("");
+		menu.add("Choose an option");
+		menu.endOfRow();
+		menu.add("1");
+		menu.add("Show Rules");
+		menu.endOfRow();
+		menu.add("2");
+		menu.add("Vs. Player");
+		menu.endOfRow();
+		menu.add("3");
+		menu.add("Vs. CPU");
+		menu.endOfRow();
+		menu.add("4");
+		menu.add("PH");
+		menu.endOfRow();
+		menu.add("5");
+		menu.add("Exit");
+		menu.endOfRow();
+
+		menu.setAlignment(2, TextTable::Alignment::RIGHT);
+		cout << menu;
+	}
+};
 
 // function declaration
 void TitleDrop()
@@ -102,14 +138,9 @@ void UpdateBoard(vector<string>& boardValues, int position, string value)
 	{
 		if (i == position)
 		{
-			boardValues[i - 1] = value;
+			boardValues[static_cast<std::vector<std::string, std::allocator<std::string>>::size_type>(i) - 1] = value;
 		}
 	}
-}
-
-void ShowRules()
-{
-	cout << "This is an interpretation of the Classic TicTacToe game, where 2 players duel in a 4x4 square matrix.\n The goal of tic-tac-toe is to be the first player to get three in a row on a 3-by-3 grid or four in a row in a 4-by-4 grid. \n To start, one player draws a board, creating a grid of squares, usually 3 - by - 3 or 4 - by - 4. \n In a 3-by-3 grid game, the player who is playing 'X' always goes first. Players alternate placing Xs and Os on the board until either player has three in a row, horizontally, vertically, or diagonally or until all squares on the grid are filled. \n If a player is able to draw three Xs or three Os in a row, then that player wins. If all squares are filled and neither player has made a complete row of Xs or Os, then the game is a draw.";
 }
 
 void BeginPlay()
@@ -151,5 +182,8 @@ void BeginPlay()
 // main function and testing ground
 int main()
 {
-	BeginPlay();
+	//BeginPlay();
+	MenuClass MainMenu;
+	MainMenu.DisplayMenu();
+	return 0;
 }
